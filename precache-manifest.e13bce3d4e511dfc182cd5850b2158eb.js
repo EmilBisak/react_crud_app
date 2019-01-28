1eee1baada2bca2448621a068d20ffdb1adf3ed6
EmilBisak <bisak.emil@gmail.com>
2019-01-28 02:03:19 +0100

    Updates

---
+++ precache-manifest.e13bce3d4e511dfc182cd5850b2158eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53bb4462cadbd5cdd470",
+    "url": "/react_crud_app/static/js/runtime~main.53bb4462.js"
+  },
+  {
+    "revision": "9c375edf1eba6d01eb45",
+    "url": "/react_crud_app/static/js/main.9c375edf.chunk.js"
+  },
+  {
+    "revision": "239172ac206b25204c1e",
+    "url": "/react_crud_app/static/js/1.239172ac.chunk.js"
+  },
+  {
+    "revision": "9c375edf1eba6d01eb45",
+    "url": "/react_crud_app/static/css/main.28a70588.chunk.css"
+  },
+  {
+    "revision": "239172ac206b25204c1e",
+    "url": "/react_crud_app/static/css/1.af9528db.chunk.css"
+  },
+  {
+    "revision": "c5be876f6292d035d1e842178e8fc7ec",
+    "url": "/react_crud_app/index.html"
+  }
+];
